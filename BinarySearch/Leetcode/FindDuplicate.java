b2464f95b7cd5c7bc87c0b34f7288d8492bd745f
Neo <surendarraja420@gmail.com>
2026-01-02 22:57:01 +0530

    DSA:DSA

--- BinarySearch/Leetcode/FindDuplicate.java
+++ BinarySearch/Leetcode/FindDuplicate.java
@@ -3,10 +3,28 @@ package BinarySearch.Leetcode;
 public class FindDuplicate {
     public static void main(String[] args) {
         int[] nums = {1, 3, 4, 2, 2};
-        int left=0,right=nums.length-1;
-        while(left<right){
-            int mid=left+(right-left)/2;
 
+        int left = 1;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = 0;
+
+            // Count numbers <= mid
+            for (int num : nums) {
+                if (num <= mid) {
+                    count++;
+                }
+            }
+
+            if (count > mid) {
+                right = mid;      // duplicate in left half
+            } else {
+                left = mid + 1;   // duplicate in right half
+            }
         }
+
+        System.out.println("Duplicate number: " + left);
     }
 }

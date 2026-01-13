7a8a1eed93184054af284dcc3de3f8bd50640d41
Neo <surendarraja420@gmail.com>
2026-01-13 23:05:22 +0530

    DSA:DSA

--- DynamicProgramming/ClimbStairs.java
+++ DynamicProgramming/ClimbStairs.java
@@ -1,13 +1,26 @@
 package DynamicProgramming;
 
 public class ClimbStairs {
-    public static void main(String[] args) {
-        int n=3;
-        int[] dp=new int[n+1];
-        dp[1]=1;dp[2]=2;
-        for (int i = 3; i <= n ; i++) {
-            dp[i]=dp[i-1]+dp[i-2];
+
+    static int climbStairs(int n) {
+        if (n <= 2) return n;
+        int past = 1, prev = 2;
+        for (int i = 3; i <= n; i++) {
+            int curr = past + prev;
+            past = prev;
+            prev = curr;
         }
-        System.out.println(dp[n]);
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(climbStairs(n));
     }
 }
